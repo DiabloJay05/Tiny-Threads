77ac3568467a07897ca75a16a71a03185e656443
DiabloJay05 <joshua.a.wilson05@gmail.com>
2025-04-14 22:30:29 +0200

    Add files via upload

---
+++ Tiny Threads V2/products.js
@@ -0,0 +1,46 @@
+const products = [
+    {
+        "id": 1,
+        "name": "Dino",
+        "price": 50,
+        "description": "Boys Dinosuar Flap Tee",
+        "image": "images/Dino.jpg"
+    },    
+    {
+        "id": 2,
+        "name": "Dig",
+        "price": 80,
+        "description": "Boys Dig Flap Tee",
+        "image": "images/Dig.jpg"
+    },
+    {
+        "id": 3,
+        "name": "DBZ",
+        "price": 50,
+        "description": "Boys DBZ Graphic Tee",
+        "image": "images/DBZ.jpg"
+    },
+    {
+        "id": 4,
+        "name": "NASA",
+        "price": 50,
+        "description": "Boys NASA Graphic Tee",
+        "image": "images/NASA.jpg"
+    },
+    {
+        "id": 5,
+        "name": "Pizza",
+        "price": 50,
+        "description": "Boys Pizza Graphic Tee",
+        "image": "images/Pizza.jpg"
+    },    
+    {
+        "id": 6,
+        "name": "Skull",
+        "price": 50,
+        "description": "Boys Printed Tee",
+        "image": "images/Skull.jpg"
+    },
+    
+];
+export default products;
